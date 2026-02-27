51959021a1add3127f6e954c31b2fd643b645270
izabela <40214340+izabela-am@users.noreply.github.com>
2026-02-27 10:25:00 -0300

    Update keybindings.lua

--- nvim/lua/keybindings.lua
+++ nvim/lua/keybindings.lua
@@ -4,9 +4,11 @@ vim.keymap.set("n", "<Leader>w", "<Cmd>update<CR>", { desc = "Update Buffer" })
 vim.keymap.set("n", "<Leader>qa", "<Cmd>qa<CR>", { desc = "Quit All" })
 vim.keymap.set("n", "<Leader>gl", "<C-w>h", { desc = "Go Left" })
 vim.keymap.set("n", "<Leader>gr", "<C-w>l", { desc = "Go Right" })
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
 
 vim.keymap.set("n", "<Tab>", ":bnext<CR>", { desc = "Next Buffer" })
 vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", { desc = "Previous Buffer" })
+
 -- none-ls
 vim.keymap.set("n", "<Leader>gf", vim.lsp.buf.format, { desc = "Format Code" })
 
